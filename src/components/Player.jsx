6e0eec37b1b3196effd7eedd90b134601e13b3ee
Jenny Carroll <jennygc@telus.net>
2023-03-17 19:56:22 -0600

    pass props from player list to player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Player () {
+function Player (props) {
   return (
     <article className="Player">
-    <h1>John <span>Secret</span> Doe</h1>
+    <h1>{props.firstName}<span>Secret</span> {props.lastName}</h1>
     {/* To be shown when there are no wins */}<h2 className="zero">Currently with no wins</h2>
     {/* To be shown when there is 1 win */}<h2>Currently at 1 win</h2>
     {/* To be shown when there is more than one win */}<h2>Currently at 1+ wins</h2>
